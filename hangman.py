4f4299764367b79b446e977093090e9b9c09ac2e
Ben Tristem <github@bentristem.com>
2018-05-09 11:56:36 +0100

    Refactored

--- hangman.py
+++ hangman.py
@@ -1,11 +1,35 @@
 import csv
 
-words = set()
+words = set()  # keeping so that subsequent plays have no repeat words
 
 
-with open('words.csv') as file:
-    reader = csv.reader(file)
-    for row in reader:
-        words.add(row[0])
+def play_games():
+    load_words_from_file()
+    print('Loaded ' + str(len(words)) + ' words')
 
-print(words)
+    # introduce game
+    # while user want to play
+        # pick a random word
+        # present blanks to user
+        # test user input
+
+
+def load_words_from_file():
+    with open('10k_words.txt') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            add_if_longer_than_min(row, 4)
+
+
+def add_if_longer_than_min(row, minimum):
+    first_column = row[0]
+    if len(first_column) >= minimum:
+        words.add(first_column)
+
+
+if __name__ == '__main__':
+    play_games()
+
+
+# TODO add simple tests
+# TODO find better way of interacting with user

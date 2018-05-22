b1c4d73f4f544228c230a99cffa3cfdd3625f5db
Ben Tristem <github@bentristem.com>
2018-05-22 13:13:01 +0100

    Undoing execution flow spaghetti (nom, nom, nom)

--- hangman.py
+++ hangman.py
@@ -8,15 +8,23 @@ STARTING_LIVES = 5
 WORD_BANK = '10k_words.txt'
 
 # private instance variables for state
-current_lives = STARTING_LIVES
+lives = STARTING_LIVES
 word_length = 0
 hidden_word = []
 display_word = []
 
 
-def play_game():
-    global current_lives, word_length, hidden_word
-    current_lives = STARTING_LIVES
+def play_games():
+    wants_to_play = True
+
+    while wants_to_play:
+        play_single_game()
+        wants_to_play = prompt_to_play_again()
+
+
+def play_single_game():
+    global lives, word_length, hidden_word
+    lives = STARTING_LIVES
 
     introduce_game()  # View layer
     word_length = prompt_for_level()
@@ -25,77 +33,14 @@ def play_game():
     print('Time to start guessing your', len(hidden_word), 'letter word', ''.join(display_word))
 
     while display_word != hidden_word:
-        check_lives()
-        print('You have', current_lives, ' left')
+        if lives == 0:
+            print('You have run out of lives...')
+            return  # note leave the game (see break, return, continue differences)
+        print('You have', lives, 'left')  # note , adds spaces for you
         letter = get_player_guess()
         update_display_word(letter)
 
     print('Waahoooo You Guessed it!')
-    does_player_want_to_play_again()
-
-
-def check_lives():
-    if current_lives > 0:
-        return
-    else:
-        game_over()
-
-
-def does_player_want_to_play_again():
-    answer = input('Do you wish to play again? Y/N')
-
-    if answer.upper() == 'Y':
-        current_lives
-        print()
-        print('Staring new game...')
-        time.sleep(2 * SLEEP_TIME)
-        play_game()
-    elif answer.upper() == 'N':
-        print()
-        print('Goodbye')
-        power_down = list('..........')
-        while len(power_down) > 0:
-            print(''.join(power_down))
-            time.sleep(SLEEP_TIME / 2)
-            power_down.pop()
-        quit()
-    else:
-        print('Invalid input')
-        does_player_want_to_play_again()
-
-
-def game_over():
-    print('You have run out of lives...')
-    does_player_want_to_play_again()
-
-
-def get_player_guess():
-    print()
-    letter = input('Please guess a letter: ').upper() # get player input, force lowercase for the moment
-    if not is_valid_guess(letter):
-        print("Opps, that's not right. Please try again.")  # Used " this time because of the ' in sentence
-        get_player_guess()
-    return letter
-
-
-def update_display_word(letter):
-    if letter in hidden_word:
-        print('Found A Letter')
-        place_letters(letter)
-    else:
-        global current_lives
-
-        print('Not in this word, try again...')
-        print(''.join(display_word))
-
-        current_lives -= 1
-
-
-def place_letters(letter):
-    for position_in_word in range(len(hidden_word)):
-        if hidden_word[position_in_word] == letter:
-            display_word[position_in_word] = letter
-            print(''.join(display_word))  # TODO if there are multiple letters in loops and displays multiple display_words
 
 
 def introduce_game():
@@ -112,6 +57,64 @@ def introduce_game():
     print('What level would you like to play?')
     print()
     time.sleep(SLEEP_TIME)
+
+
+def check_lives():
+    if lives > 0:
+        return
+    else:
+        game_over()
+
+
+def prompt_to_play_again():
+    while True:  # keep on asking!
+        answer = input('Do you wish to play again? Y/N')
+
+        if answer.upper() == 'Y':
+            print()
+            print('Starting new game...')
+            time.sleep(2 * SLEEP_TIME)
+            return True
+        elif answer.upper() == 'N':
+            print()
+            print('Goodbye')
+            power_down = list('..........')
+            while len(power_down) > 0:
+                print(''.join(power_down))
+                time.sleep(SLEEP_TIME / 2)
+                power_down.pop()
+            return False
+        else:
+            print('Invalid input')
+
+
+def get_player_guess():
+    print()
+    letter = input('Please guess a letter: ').upper() # get player input, force lowercase for the moment
+    if not is_valid_guess(letter):
+        print("Opps, that's not right. Please try again.")  # Used " this time because of the ' in sentence
+        get_player_guess()
+    return letter
+
+
+def update_display_word(letter):
+    if letter in hidden_word:
+        print('Found A Letter')
+        place_letters(letter)
+    else:
+        global lives
+
+        print('Not in this word, try again...')
+        print(''.join(display_word))
+
+        lives -= 1
+
+
+def place_letters(letter):
+    for position_in_word in range(len(hidden_word)):
+        if hidden_word[position_in_word] == letter:
+            display_word[position_in_word] = letter
+            print(''.join(display_word))  # TODO if there are multiple letters in loops and displays multiple display_words
 
 
 def prompt_for_level():
@@ -178,8 +181,4 @@ def is_valid_guess(letter_guessed):
 
 
 if __name__ == '__main__':
-    import doctest
-
-    # doctest.testmod()  # for in-line tests
-    # doctest.testfile("hangman_examples.md")
-    play_game()
+    play_games()
